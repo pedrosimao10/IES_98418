427746ff79ea9372aa52002accfa0b13627f4d7f
Pedro <pedro.simao10@ua.pt>
2021-10-18 19:22:31 +0100

    Initial project setup for exercise 1_3

---
+++ src/main/java/com/mycompany/weather/WeatherStarter.java
@@ -0,0 +1,65 @@
+package com.mycompany.weather;
+
+import java.util.logging.Logger;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import java.lang.StringBuilder;
+
+/**
+ * demonstrates the use of the IPMA API for weather forecast
+ */
+public class WeatherStarter {
+    private static int cityCode = 1010500;
+    //private static final int CITY_ID_AVEIRO = 1010500;
+    /*
+    loggers provide a better alternative to System.out.println
+    https://rules.sonarsource.com/java/tag/bad-practice/RSPEC-106
+     */
+    private static final Logger logger = Logger.getLogger(WeatherStarter.class.getName());
+
+    public static void  main(String[] args ) {
+
+        if (args.length > 0){
+            try {
+                cityCode = Integer.parseInt(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /*
+        get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+         */
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        IpmaService service = retrofit.create(IpmaService.class);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(cityCode);
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                StringBuilder sb = new StringBuilder();
+                String log = sb.append("max temp for today: ")
+                                .append(forecast.getData().listIterator().next().getTMax())
+                                .append('\n')
+                                .append("min temp for today: ")
+                                .append(forecast.getData().listIterator().next().getTMin())
+                                .toString();
+                logger.info(log);
+            } else {
+                logger.info( "No results!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
