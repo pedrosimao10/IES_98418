ff02bce9f30a2fe6797abec5f311d59093ad11e3
Pedro <pedro.simao10@ua.pt>
2021-10-18 19:29:07 +0100

    Initial  project setup for exercise 1_3

--- lab1/README.md
+++ lab1/README.md
@@ -53,6 +53,28 @@ o resultado obtido foi o seguinte:
 out 12, 2021 8:17:47 DA TARDE com.mycompany.weather.WeatherStarter main
 INFO: max temp for today: 27.0
      
+Utilizou-se StringBuilder de forma a ser possível pesquisar a cidade pelo city code.
+Obteu-se o seguinte resultado:
+
+[INFO] Scanning for projects...
+[INFO]
+[INFO] ----------------< com.mycompany.weather:MyWeatherRadar >----------------
+[INFO] Building MyWeatherRadar 1.0-SNAPSHOT
+[INFO] --------------------------------[ jar ]---------------------------------
+[INFO]
+[INFO] --- exec-maven-plugin:3.0.0:java (default-cli) @ MyWeatherRadar ---
+WARNING: An illegal reflective access operation has occurred
+WARNING: Illegal reflective access by retrofit2.Platform (file:/C:/Users/pedro/.m2/repository/com/squareup/retrofit2/retrofit/2.9.0/retrofit-2.9.0.jar) to constructor java.lang.invoke.MethodHandles$Lookup(java.lang.Class,int)
+WARNING: Please consider reporting this to the maintainers of retrofit2.Platform
+WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+WARNING: All illegal access operations will be denied in a future release
+out 18, 2021 6:55:14 DA TARDE com.mycompany.weather.WeatherStarter main
+INFO: max temp for today: 24.4
+min temp for today: 16.8
+
+Comando para correr o programa no maven com argumentos: mvn exec:java -Dexec.mainClass="what to put here?" -Dexec.args="arg0 arg1 arg2"~
+
 # ---- Exercício 1.3 ---------
 
->>>>>>> 610cf497e58704263251a7d04e6ae995293ac144
+.gitignore - Ficheiros/Pastas que não devem ser incluídas no repositório
+
--- lab1/lab1_2/MyWeatherRadar/src/main/java/com/mycompany/weather/WeatherStarter.java
+++ lab1/lab1_2/MyWeatherRadar/src/main/java/com/mycompany/weather/WeatherStarter.java
@@ -6,13 +6,14 @@ import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import java.lang.StringBuilder;
 
 /**
  * demonstrates the use of the IPMA API for weather forecast
  */
 public class WeatherStarter {
-
-    private static final int CITY_ID_AVEIRO = 1010500;
+    private static int cityCode = 1010500;
+    //private static final int CITY_ID_AVEIRO = 1010500;
     /*
     loggers provide a better alternative to System.out.println
     https://rules.sonarsource.com/java/tag/bad-practice/RSPEC-106
@@ -20,6 +21,14 @@ public class WeatherStarter {
     private static final Logger logger = Logger.getLogger(WeatherStarter.class.getName());
 
     public static void  main(String[] args ) {
+
+        if (args.length > 0){
+            try {
+                cityCode = Integer.parseInt(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         /*
         get a retrofit instance, loaded with the GSon lib to convert JSON into objects
@@ -30,15 +39,21 @@ public class WeatherStarter {
                 .build();
 
         IpmaService service = retrofit.create(IpmaService.class);
-        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(cityCode);
 
         try {
             Response<IpmaCityForecast> apiResponse = callSync.execute();
             IpmaCityForecast forecast = apiResponse.body();
 
             if (forecast != null) {
-                logger.info( "max temp for today: " + forecast.getData().
-                        listIterator().next().getTMax());
+                StringBuilder sb = new StringBuilder();
+                String log = sb.append("max temp for today: ")
+                                .append(forecast.getData().listIterator().next().getTMax())
+                                .append('\n')
+                                .append("min temp for today: ")
+                                .append(forecast.getData().listIterator().next().getTMin())
+                                .toString();
+                logger.info(log);
             } else {
                 logger.info( "No results!");
             }
